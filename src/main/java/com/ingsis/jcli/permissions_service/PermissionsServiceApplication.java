cb7267540d588d6c470d1d2713d4ddff67d2e883
inesgassiebayle <110676544+inesgassiebayle@users.noreply.github.com>
2024-09-19 17:09:47 -0300

    Add files via upload

---
+++ src/main/java/com/ingsis/jcli/permissions_service/PermissionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.permissions_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsis/jcli/permissions_service/PermissionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.jcli.permissions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
