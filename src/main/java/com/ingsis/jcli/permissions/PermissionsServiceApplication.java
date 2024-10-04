14f110142d72e72a293b620c09b53017ca62277b
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2024-10-03 22:26:59 -0300

    Remove Dotenv

--- src/main/java/com/ingsis/jcli/permissions/PermissionsServiceApplication.java
+++ src/main/java/com/ingsis/jcli/permissions/PermissionsServiceApplication.java
@@ -1,6 +1,5 @@
 package com.ingsis.jcli.permissions;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,17 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @Generated
 public class PermissionsServiceApplication {
   public static void main(String[] args) {
-    loadEnv();
     SpringApplication.run(PermissionsServiceApplication.class, args);
-  }
-
-  public static void loadEnv() {
-    Dotenv dotenv = Dotenv.configure().load();
-    System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-    System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-    System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
-    System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
-    System.setProperty("PORT", dotenv.get("PORT"));
-    System.setProperty("POSTGRES_HOST", dotenv.get("POSTGRES_HOST"));
   }
 }
