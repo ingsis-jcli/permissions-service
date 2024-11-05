a8d1ee70c140bc68981e0d0cc9a1d71be75592e0
inesgassiebayle <igassiebayle@mail.austral.edu.ar>
2024-11-05 12:38:21 -0300

    test get auth0 admin token, match auth0 documentation

--- src/main/java/com/ingsis/jcli/permissions/services/Auth0Service.java
+++ src/main/java/com/ingsis/jcli/permissions/services/Auth0Service.java
@@ -8,21 +8,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class Auth0Service {
 
-  @Value("${spring.security.oauth2.resourceserver.jwt.client-id}")
-  private String clientId;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.client-secret}")
-  private String clientSecret;
-
-  @Value("${auth0.audience}")
-  private String audience;
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-  private String authServerUri;
-
   private final Auth0Client auth0Client;
 
-  public Auth0Service() {
+  public Auth0Service(
+      @Value("${spring.security.oauth2.resourceserver.jwt.client-id}") String clientId,
+      @Value("${spring.security.oauth2.resourceserver.jwt.client-secret}") String clientSecret,
+      @Value("${auth0.audience}") String audience,
+      @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String authServerUri) {
     this.auth0Client =
         new Auth0Client(new RestTemplate(), authServerUri, clientId, clientSecret, audience);
   }
