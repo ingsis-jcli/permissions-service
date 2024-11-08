257b9d116ea8bea57bb13be4f23115350a0fbdb5
ClaraLopez1 <mclopez@mail.austral.edu.ar>
2024-11-08 18:31:17 -0300

    adding header of CORRELATION_ID_HEADER

--- src/main/java/com/ingsis/jcli/permissions/auth0/AuthFeignInterceptor.java
+++ src/main/java/com/ingsis/jcli/permissions/auth0/AuthFeignInterceptor.java
@@ -1,8 +1,12 @@
 package com.ingsis.jcli.permissions.auth0;
 
+import static com.ingsis.jcli.permissions.CorrelationIdFilter.CORRELATION_ID_HEADER;
+import static com.ingsis.jcli.permissions.CorrelationIdFilter.CORRELATION_ID_KEY;
+
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.MDC;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
@@ -21,5 +25,11 @@ public class AuthFeignInterceptor implements RequestInterceptor {
       template.header(
           HttpHeaders.AUTHORIZATION, httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION));
     }
+    String correlationId = MDC.get(CORRELATION_ID_KEY);
+    if (correlationId != null) {
+      template.header(CORRELATION_ID_HEADER, correlationId);
+    }
   }
 }
+
+
